d5076e8002fde62f4a0972212e2e94157fd66b03
Sheldon <Sheldon.Wei.-ND@disney.com>
2024-07-15 23:27:31 +0800

    2nd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use bracket_lib::prelude::{BError, BTerm, BTermBuilder, main_loop};
+use bracket_lib::terminal::GameState;
+
+fn main() -> BError{
+    let c = BTermBuilder::simple80x50().with_title("Flappy Donkey").build()?;
+    main_loop(c, State {y:0})
+}
+struct State {
+    y: i32
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        self.y = self.y +1;
+        ctx.cls();
+        ctx.print(1, self.y, "Hello bterm");
+    }
 }
